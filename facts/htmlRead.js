c5028e1687988a5fcd2e4bd5500faf56599ee611
taniya142 <taniyagehlaut14@gmail.com>
2021-04-27 21:10:40 +0530

    add more

--- facts/htmlRead.js
+++ facts/htmlRead.js
@@ -10,5 +10,22 @@ let ch=cheerio.load(htmlKaData);
 let h1kaObject=ch("h1");
 // console.log(h1kaObject);
 
-let h1kaData=ch("h1").text();
+
+// .text() function is only for ch that is for big object 
+let h1kaData=ch("h1").text();     
 // console.log(h1kaData);
+
+// let pTag=ch("p").text();
+// console.log(pTag);
+
+let pTag=ch("p");
+let lastpTag=ch(pTag["2"]).text();
+// console.log(lastpTag);
+
+// selector
+let pInsideUl=ch("ul li p").text();    
+// console.log(pInsideUl);
+
+console.log(ch("p").length);
+
+console.log(ch(".text.main").text());
