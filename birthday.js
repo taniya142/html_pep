1111b9d4304f3604155fd8e45cb0969854e91090
taniya142 <taniyagehlaut14@gmail.com>
2021-05-19 16:00:14 +0530

    birthdates

---
+++ birthday.js
@@ -0,0 +1,36 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+request("https://www.espncricinfo.com/series/ipl-2021-1249214/punjab-kings-vs-delhi-capitals-29th-match-1254086/full-scorecard",callback);
+
+function callback(error,response,html){
+    if(!error){
+        const manipulationTool = cheerio.load(html);
+
+        let allPlayers = manipulationTool(".Collapsible__contentInner tbody a");
+
+
+            for(let j=0; j<allPlayers.length; j++){
+                let playerName = manipulationTool(allPlayers[j]).text();
+                let playerLink = manipulationTool(allPlayers[j]).attr("href");
+
+                // console.log("---------------------");
+
+                birthDates(playerName,"https://www.espncricinfo.com/" + playerLink);
+                
+            }
+    }
+}
+
+
+function birthDates(name, link){
+    request(link,function(error,response,html){
+        const manipulationTool = cheerio.load(html);
+
+        let date = manipulationTool(".player-card-description.gray-900");
+
+        console.log(name + " -----> " + manipulationTool(date[1]).text());
+        
+    });
+    
+}
